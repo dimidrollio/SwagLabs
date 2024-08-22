21fc8255c0cd12f1c9cbec34eeb5d61991b789aa
Dmytro Simonov <dmytro.simonov.simiq@gmail.com>
2024-08-22 12:05:21 +0300

    Implemented abstract factory principle and remade testData

---
+++ SwagLabs/Driver/EdgeDriver/EdgeDriverManager.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace SwagLabs.Driver.Edge
+{
+    public class EdgeDriverManager : IDriverManager
+    {
+        private EdgeDriver? _driver;
+        public IWebDriver CreateDriver()
+        {
+            if (_driver is null)
+            {
+                _driver = new EdgeDriver();
+                return _driver;
+
+            }
+            else
+            {
+                return GetDriver();
+            }
+        }
+
+        public IWebDriver GetDriver()
+        {
+            if (_driver is null)
+            {
+                CreateDriver();
+            }
+
+            return _driver;
+        }
+
+        public void QuitDriver()
+        {
+            if (_driver is not null)
+            {
+				_driver.Quit();
+				_driver = null;
+			}           
+        }
+    }
+}
---
+++ SwagLabs/Driver/IDriverFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace SwagLabs.Driver
+{
+	public interface IDriverFactory
+	{
+		public IDriverManager CreateDriverManager(string browserName);
+	}
+}
